c428ae12e878537248bc44e7998a44bcc3f06fc9
Jose Antonio <campi1112@gmail.com>
2018-12-26 17:29:49 +0100

    inicial commit

---
+++ pages/axios.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="(usuario, key) in usuarios" :key="key">
+      <div>{{ usuario.displayName }} {{ usuario.phone }}</div>
+      {{key}}
+      <ol>
+        <li v-for="(viaje, key, i) in usuario.viajes" :key="key">
+          <div>viaje.km_sal: {{viaje.km_sal}}</div>
+          <div>key: {{key}}, i:{{i}}</div>
+        </li>
+      </ol>
+    </li>
+  </ul>
+</template>
+
+<script>
+import fetch from 'isomorphic-fetch';
+import axios from 'axios';
+export default {
+  async asyncData() {
+    const response = await axios.get(
+      "https://milab-519f4.firebaseapp.com/usuarios.json"
+    );
+    console.log(response.data);
+    return {usuarios: response.data}
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="(usuario, key) in usuarios" :key="key">
+      <div>{{ usuario.displayName }} {{ usuario.phone }}</div>
+      {{key}}
+      <ol>
+        <li v-for="(viaje, key, i) in usuario.viajes" :key="key">
+          <div>viaje.km_sal: {{viaje.km_sal}}</div>
+          <div>key: {{key}}, i:{{i}}</div>
+        </li>
+      </ol>
+    </li>
+  </ul>
+</template>
+
+<script>
+import fetch from 'isomorphic-fetch';
+export default {
+  async asyncData() {
+    const response = await fetch(
+      "https://milab-519f4.firebaseapp.com/usuarios.json"
+    );
+    const usuarios = await response.json();
+    //console.log(usuarios);
+    return { usuarios };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
